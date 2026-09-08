9983b1cbd3ca72a627ec7551307c1669d0861453
MichalisMichael26 <mike-rhapsody@hotmail.com>
2026-09-08 11:29:51 +0300

    Create marvel_data.py

---
+++ marvel_data.py
@@ -0,0 +1,136 @@
+# ============================================================
+# MARVEL
+# Μεμονωμένες Marvel εκδόσεις εκτός Marvel Graphic Novel
+# ============================================================
+
+MARVEL_COMICS = [
+    {
+        "number": 1,
+        "title": "Avengers: The Initiative",
+        "search_title": "Avengers The Initiative Marvel",
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 2,
+        "title": "Silver Surfer: Requiem",
+        "search_title": (
+            "Silver Surfer Requiem "
+            "J Michael Straczynski Esad Ribic"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 3,
+        "title": "Thor: Η Εποχή του Κεραυνού",
+        "search_title": (
+            "Thor Ages of Thunder Matt Fraction"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 4,
+        "title": "Ultimate X-Men / Fantastic Four",
+        "search_title": (
+            "Ultimate X-Men Fantastic Four Marvel"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 5,
+        "title": "Marvel Zombies: Ακόρεστη Πείνα",
+        "search_title": (
+            "Marvel Zombies Robert Kirkman Sean Phillips"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 6,
+        "title": "Civil War",
+        "search_title": (
+            "Marvel Civil War Mark Millar Steve McNiven"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 7,
+        "title": "Wolverine: Old Man Logan",
+        "search_title": (
+            "Wolverine Old Man Logan "
+            "Mark Millar Steve McNiven"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 8,
+        "title": "Infinity Gauntlet",
+        "search_title": (
+            "Infinity Gauntlet Jim Starlin George Perez"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 9,
+        "title": "Hulk: WWH - World War Hulk",
+        "search_title": (
+            "World War Hulk Greg Pak John Romita Jr"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 10,
+        "title": "Avengers Assemble",
+        "search_title": (
+            "Avengers Assemble Marvel graphic novel"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 11,
+        "title": "Venom Rex",
+        "search_title": "Venom Rex Marvel",
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 12,
+        "title": "Amazing Spider-Man: Red Goblin",
+        "search_title": (
+            "Amazing Spider-Man Red Goblin "
+            "Dan Slott Stuart Immonen"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 13,
+        "title": "Secret Wars",
+        "search_title": (
+            "Marvel Secret Wars graphic novel"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 14,
+        "title": "Ultimate Iron Man II",
+        "search_title": (
+            "Ultimate Iron Man II Orson Scott Card"
+        ),
+        "image": "",
+        "owned": True,
+    },
+]
+
+
+def get_marvel_comics():
+    return MARVEL_COMICS

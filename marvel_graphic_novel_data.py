087344a873cac6d2d2081effacab2d08b5c76707
MichalisMichael26 <mike-rhapsody@hotmail.com>
2026-09-08 11:30:30 +0300

    Create marvel_graphic_novel_data.py

---
+++ marvel_graphic_novel_data.py
@@ -0,0 +1,172 @@
+# ============================================================
+# MARVEL GRAPHIC NOVEL COLLECTION
+# Issues 1 - 120
+# ============================================================
+
+MARVEL_GRAPHIC_NOVEL_TITLES = [
+    "Amazing Spider-Man: Η Επιστροφή - Coming Home",
+    "Iron Man: Extremis",
+    "New Avengers: Απόδραση - Breakout",
+    "Captain America: Εκτός Χρόνου - Out of Time",
+    "Deadpool: Ο Πόλεμος του Γουέιντ Γουίλσον - Wade Wilson's War",
+    "X-Men: Το Χάρισμα - Astonishing X-Men: Gifted",
+    "Thor: Αναγέννηση - Thor: Reborn",
+    "Hulk: Σιωπηλές Κραυγές - Silent Screams",
+    "Guardians of the Galaxy: Κοσμικοί Εκδικητές - Cosmic Avengers",
+    "Spider-Man: Η Γέννηση του Βένομ - Birth of Venom",
+    "Captain America: Στρατιώτης του Χειμώνα - Winter Soldier",
+    "Fantastic Four: Ασύλληπτη Παγίδα - Unthinkable",
+    "Wolverine",
+    "Daredevil: Αναγέννηση - Born Again",
+    "She-Hulk: Μια Μοναδική Ηρωίδα - Single Green Female",
+    "Doctor Strange: Άγνωστοι Τόποι, Άγνωστοι Καιροί",
+    "The Incredible Hulk: Πλανήτης Χαλκ, Μέρος Πρώτο",
+    "Captain Britain: Ένας Παράλογος Κόσμος - A Crooked World",
+    "Avengers: Μάχη στο Χρόνο, Μέρος Πρώτο - Avengers Forever Part 1",
+    "The Amazing Spider-Man: Το Τελευταίο Κυνήγι του Κρέιβεν",
+    "The Incredible Hulk: Πλανήτης Χαλκ, Μέρος Δεύτερο",
+    "New X-Men: Ε Όπως Εξάλειψη - E is for Extinction",
+    "Μυστικός Πόλεμος - Secret War",
+    "Avengers: Μάχη στο Χρόνο, Μέρος Δεύτερο - Avengers Forever Part 2",
+    "Captain America: Νέο Καθεστώς - A New Deal",
+    "Marvels",
+    "Avengers: Διάλυση - Avengers Disassembled",
+    "The Punisher: Η Επιστροφή, Μέρος Πρώτο - Welcome Back Frank Part 1",
+    "Ultimate Spider-Man: Δύναμη και Ευθύνη - Power and Responsibility",
+    "Marvel Super Heroes: Μυστικοί Πόλεμοι, Μέρος Πρώτο",
+    "The Mighty Thor: Αναζητώντας τους Θεούς - In Search of the Gods",
+    "Astonishing X-Men: Κίνδυνος - Dangerous",
+    "The Punisher: Η Επιστροφή, Μέρος Δεύτερο - Welcome Back Frank Part 2",
+    "Fantastic Four: Απόλυτη Δράση - Authoritative Action",
+    "Fallen Son: Ο Θάνατος του Captain America",
+    "Marvel Super Heroes: Μυστικοί Πόλεμοι, Μέρος Δεύτερο",
+    "Iron Man: Δαίμονας στο Μπουκάλι - Demon in a Bottle",
+    "The Ultimates: Υπεράνθρωποι - Super-Human",
+    "Spider-Man: Blue",
+    "X-Men: Αυτοκρατορική Εισβολή - New X-Men: Imperial",
+    "Οίκος του Μ - House of M",
+    "Wolverine: Η Αρχή - Origin",
+    "The Ultimates: Εθνική Ασφάλεια - Homeland Security",
+    "Thor: Ο Τελευταίος Βίκινγκ - The Last Viking",
+    "Η Επιστροφή του Θάνος - The Thanos Imperative",
+    "Spider-Gwen: Καταζητούμενη; - Most Wanted?",
+    "Daredevil: Φύλακας Δαίμονας - Guardian Devil",
+    "Amazing Spider-Man: Revelations and Until The Stars Turn Cold",
+    "Eternals",
+    "Fantastic Four: The End",
+    "Jessica Jones: Uncaged",
+    "The Uncanny X-Men: Second Genesis",
+    "Ghost Rider: Road to Damnation",
+    "The Avengers: Birth of Ultron",
+    "Marvel Zombies",
+    "Marvel Knights Spider-Man Vol. 1: Down Among The Dead Men",
+    "Secret Avengers Vol. 1: Mission to Mars",
+    "Fantastic Four: The Coming of Galactus",
+    "Marvel Origins: The 60s",
+    "Marvel Knights Spider-Man Vol. 2: Venomous",
+
+    # ========================================================
+    # ΜΕΡΟΣ ΔΕΥΤΕΡΟ
+    # ========================================================
+
+    "Black Panther: Ποιος Είναι ο Μαύρος Πάνθηρας;",
+    "Captain America & The Falcon: Μυστική Αυτοκρατορία",
+    "Wolverine: Όπλο X",
+    "Marvel 1602",
+    "Deadpool: Οι Μυστικά Μυστικοί Πόλεμοι",
+    "Hulk: Παγκόσμιος Εχθρός - World War Hulk",
+    "Captain Britain and MI13: Η Επίθεση των Βαμπίρ",
+    "Captain America: Ο Εκλεκτός - The Chosen",
+    "Wolverine: Ο Γέρος Λόγκαν - Old Man Logan",
+    "Μυστική Εισβολή - Secret Invasion",
+    "Doctor Strange: Ο Όρκος - The Oath",
+    "Thunderbolts: Έμπιστα Τέρατα - Faith in Monsters",
+    "Venom",
+    "Ο Γιος του Μ - Son of M",
+    "Avengers: Επανένωση",
+    "X-Men: Το Λυκόφως των Μεταλλαγμένων",
+    "Το Βασίλειο της Σκιάς - Shadowland",
+    "Iron Man: The Tragedy and the Triumph",
+    "X-Men: Schism",
+    "The Life and Death of Captain Marvel Part 1",
+    "The Life and Death of Captain Marvel Part 2",
+    "Ultimate Death of Spider-Man",
+    "Thor: Tales of Asgard",
+    "Siege",
+    "The Incredible Hulk: The Monster Unleashed",
+    "Nick Fury: Agent of S.H.I.E.L.D. Part 1",
+    "Nick Fury: Agent of S.H.I.E.L.D. Part 2",
+    "Avengers: The Children's Crusade",
+    "Daredevil: Marked for Murder",
+    "The Amazing Spider-Man: Spider-Man No More",
+    "The Mighty Thor: Ragnarok",
+    "Iron Fist: The Search for Colleen Wing",
+    "X-Men: In the Shadow of Sauron",
+    "Hulk: Scorched Earth",
+    "Doctor Strange: A Separate Reality",
+    "Silver Surfer: Origins",
+    "Spider-Island Part 1",
+    "Spider-Island Part 2",
+    "Black Panther: Panther's Rage",
+    "Marvel Horror",
+    "Fantastic Four: Doomsday",
+    "The Amazing Spider-Man: Death of the Stacys",
+    "Daredevil: Sound and Fury",
+    "The Avengers: The Korvac Saga",
+    "Iron Man: The Beginning of the End",
+    "Uncanny Avengers: The Red Shadow",
+    "Hawkeye: My Life as a Weapon",
+    "All-New Ghost Rider: Engines of Vengeance",
+    "Avengers: Avengers World",
+    "Silver Surfer: New Dawn",
+    "New Avengers: Everything Dies",
+    "Superior Spider-Man: My Own Worst Enemy",
+    "Thor: God of Thunder - The God Butcher",
+    "Infinity Vol. 1",
+    "Infinity Vol. 2",
+    "Young Avengers: Style > Substance",
+    "Death of Wolverine",
+    "Rocket Raccoon: A Chasing Tail",
+    "The Amazing Spider-Man: Spider-Verse",
+    "Civil War",
+]
+
+
+# ============================================================
+# ΤΕΥΧΗ ΠΟΥ ΔΕΝ ΕΧΟΥΜΕ
+# ============================================================
+
+MISSING_NUMBERS = {
+    96,
+}
+
+
+# ============================================================
+# ΔΗΜΙΟΥΡΓΙΑ DATA
+# ============================================================
+
+MARVEL_GRAPHIC_NOVELS = []
+
+for number, title in enumerate(
+    MARVEL_GRAPHIC_NOVEL_TITLES,
+    start=1,
+):
+    MARVEL_GRAPHIC_NOVELS.append(
+        {
+            "number": number,
+            "title": title,
+            "search_title": (
+                f"{title} Marvel Graphic Novel Collection"
+            ),
+            "image": "",
+            "owned": number not in MISSING_NUMBERS,
+        }
+    )
+
+
+# ============================================================
+# GETTER
+# ============================================================
+
+def get_marvel_graphic_novels():
+    return MARVEL_GRAPHIC_NOVELS

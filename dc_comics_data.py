68477e6f4443cc8ad1b466a540830ab683efbe2f
MichalisMichael26 <mike-rhapsody@hotmail.com>
2026-09-08 11:29:05 +0300

    Create dc_comics_data.py

---
+++ dc_comics_data.py
@@ -0,0 +1,166 @@
+# ============================================================
+# DC COMICS
+# Τίτλοι που διακρίνονται καθαρά στις φωτογραφίες της συλλογής
+# ============================================================
+
+DC_COMICS = [
+    {
+        "number": 1,
+        "title": "Batman: The Killing Joke",
+        "search_title": (
+            "Batman The Killing Joke "
+            "Alan Moore Brian Bolland"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 2,
+        "title": "Superman: Red Son",
+        "search_title": (
+            "Superman Red Son Mark Millar"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 3,
+        "title": "Batman: The Dark Knight Returns",
+        "search_title": (
+            "Batman The Dark Knight Returns "
+            "Frank Miller"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 4,
+        "title": "Justice League: Οι Μηχανές Εξέγερσης",
+        "search_title": (
+            "Justice League Origin "
+            "Geoff Johns Jim Lee"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 5,
+        "title": "Batman: Αποστολή Αυτοκτονίας",
+        "search_title": (
+            "Batman suicide mission "
+            "graphic novel DC"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 6,
+        "title": "Superman: Godfall - Η Πτώση του Θεού",
+        "search_title": (
+            "Superman Godfall "
+            "Joe Kelly Michael Turner"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 7,
+        "title": "DC Universe: Rebirth",
+        "search_title": (
+            "DC Universe Rebirth "
+            "Geoff Johns Gary Frank"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 8,
+        "title": "Suicide Squad",
+        "search_title": (
+            "Suicide Squad DC graphic novel"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 9,
+        "title": "Batman: Arkham - Scarecrow",
+        "search_title": (
+            "Batman Arkham Scarecrow DC Comics"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 10,
+        "title": "Superman: Earth One",
+        "search_title": (
+            "Superman Earth One "
+            "J Michael Straczynski Shane Davis"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 11,
+        "title": "Wonder Woman: The Hiketeia",
+        "search_title": (
+            "Wonder Woman The Hiketeia "
+            "Greg Rucka JG Jones"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 12,
+        "title": "Strange Adventures",
+        "search_title": (
+            "Strange Adventures "
+            "Tom King Mitch Gerads Doc Shaner"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 13,
+        "title": "Batman: Three Jokers",
+        "search_title": (
+            "Batman Three Jokers "
+            "Geoff Johns Jason Fabok"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 14,
+        "title": "Batman: The City of Owls",
+        "search_title": (
+            "Batman The City of Owls "
+            "Scott Snyder Greg Capullo"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 15,
+        "title": "Batman / The Flash: The Button - Η Κονκάρδα",
+        "search_title": (
+            "Batman The Flash The Button"
+        ),
+        "image": "",
+        "owned": True,
+    },
+    {
+        "number": 16,
+        "title": "Superman and Justice League America",
+        "search_title": (
+            "Superman and Justice League America DC Comics"
+        ),
+        "image": "",
+        "owned": True,
+    },
+]
+
+
+def get_dc_comics():
+    return DC_COMICS

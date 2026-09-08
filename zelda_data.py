b67ca7453d4c78dd39ab6564631ea85cea6bdc92
MichalisMichael26 <mike-rhapsody@hotmail.com>
2026-09-08 12:15:28 +0300

    Update zelda_data.py

--- zelda_data.py
+++ zelda_data.py
@@ -6,6 +6,10 @@ ZELDA_COMICS = [
     {
         "number": 1,
         "title": "The Legend of Zelda: Ocarina of Time",
+        "search_title": (
+            "The Legend of Zelda Ocarina of Time "
+            "Legendary Edition Akira Himekawa"
+        ),
         "image_url": (
             "https://d28hgpri8am2if.cloudfront.net/"
             "book_images/onix/cvr9781421589596/"
@@ -18,36 +22,11 @@ ZELDA_COMICS = [
         "number": 2,
         "title": (
             "The Legend of Zelda: "
-            "Majora's Mask / A Link to the Past"
-        ),
-        "image_url": (
-            "https://d28hgpri8am2if.cloudfront.net/"
-            "book_images/onix/cvr9781421589619/"
-            "the-legend-of-zelda-legendary-edition-"
-            "vol-3-9781421589619_hr.jpg"
-        ),
-        "owned": True,
-    },
-    {
-        "number": 3,
-        "title": (
-            "The Legend of Zelda: "
-            "The Minish Cap / Phantom Hourglass"
-        ),
-        "image_url": (
-            "https://d28hgpri8am2if.cloudfront.net/"
-            "book_images/onix/cvr9781421589626/"
-            "the-legend-of-zelda-the-minish-cap-"
-            "phantom-hourglass-legendary-edition-"
-            "9781421589626_hr.jpg"
-        ),
-        "owned": True,
-    },
-    {
-        "number": 4,
-        "title": (
-            "The Legend of Zelda: "
             "Oracle of Seasons / Oracle of Ages"
+        ),
+        "search_title": (
+            "The Legend of Zelda Oracle of Seasons "
+            "Oracle of Ages Legendary Edition Akira Himekawa"
         ),
         "image_url": (
             "https://d28hgpri8am2if.cloudfront.net/"
@@ -55,6 +34,43 @@ ZELDA_COMICS = [
             "the-legend-of-zelda-oracle-of-seasons-"
             "oracle-of-ages-legendary-edition-"
             "9781421589602_hr.jpg"
+        ),
+        "owned": True,
+    },
+    {
+        "number": 3,
+        "title": (
+            "The Legend of Zelda: "
+            "Majora's Mask / A Link to the Past"
+        ),
+        "search_title": (
+            "The Legend of Zelda Majora's Mask "
+            "A Link to the Past Legendary Edition Akira Himekawa"
+        ),
+        "image_url": (
+            "https://d28hgpri8am2if.cloudfront.net/"
+            "book_images/onix/cvr9781421589619/"
+            "the-legend-of-zelda-majora-s-mask-a-link-to-"
+            "the-past-legendary-edition-9781421589619_hr.jpg"
+        ),
+        "owned": True,
+    },
+    {
+        "number": 4,
+        "title": (
+            "The Legend of Zelda: "
+            "The Minish Cap / Phantom Hourglass"
+        ),
+        "search_title": (
+            "The Legend of Zelda The Minish Cap "
+            "Phantom Hourglass Legendary Edition Akira Himekawa"
+        ),
+        "image_url": (
+            "https://d28hgpri8am2if.cloudfront.net/"
+            "book_images/onix/cvr9781421589626/"
+            "the-legend-of-zelda-the-minish-cap-"
+            "phantom-hourglass-legendary-edition-"
+            "9781421589626_hr.jpg"
         ),
         "owned": True,
     },
